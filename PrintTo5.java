e34cb136381baa636d2174d60954689b4c7744a9
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 15:36:31 +0200

    Debugeed PrintTo5.java

--- PrintTo5.java
+++ PrintTo5.java
@@ -2,13 +2,10 @@
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
         int i = 0;
         while (i < 6) {
             System.out.println(i);
             i++;
-            
         }
-        // Complete the program's code here:
     }
  }
