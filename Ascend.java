a87ff8558a56c1b6a12445ec94b795205ce20098
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 16:01:05 +0200

    Completed aas.java

--- Ascend.java
+++ Ascend.java
@@ -3,16 +3,17 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int lim = Integer.parseInt(args[0]);
+		// get 3 random values and multiply by limit in order to get a number inside our range.
 		int a = (int)(Math.random() * lim);
 		int b = (int)(Math.random() * lim);
 		int c = (int)(Math.random() * lim);
-
+		// find the smallest number
 		int min = Math.min(a,b);
 		int finalMin = Math.min(min,c);
-
+		// find the biggest number
 		int max = Math.max(a,b);
 		int finalMax = Math.max(max,c);
-
+		// Deduct the biggest and smallest number from the sum in order to get the middle number
 		int mid = (a + b + c) - (finalMax + finalMin);
 
 		System.out.println(a + " " + b + " " + c);
