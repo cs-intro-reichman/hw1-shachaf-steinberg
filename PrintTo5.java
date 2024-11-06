0c858b3a81830a337be6626d5dbc96dacf9ad203
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 13:36:05 +0200

    completed StringTo5.java

--- PrintTo5.java
+++ PrintTo5.java
@@ -4,6 +4,11 @@ public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+            
+        }
         // Complete the program's code here:
     }
  }
