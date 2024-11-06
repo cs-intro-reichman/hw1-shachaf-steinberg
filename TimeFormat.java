ba6efc2f61672abcbb11a09efcf483d45de49c9a
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 21:11:13 +0200

    Completed TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,6 +5,8 @@ public class TimeFormat {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 		// Deduct 12 if hours is bigger then 13 to get 12-hour AM/PM format.
 		String time = "";
+		
+		
 		if (hours >= 13) {
 			time = "PM";
 			hours -= 12;
@@ -12,8 +14,14 @@ public class TimeFormat {
 			time = "AM";
 		}
 		
+		if (minutes < 10) {
+			System.out.println(hours + ":0" + minutes + " " + time);
+		} else {
 		System.out.println(hours + ":" + minutes + " " + time);
-		
-
+		}
 	}
 }
+
+
+			
+
