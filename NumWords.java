4d4be16bbdf83f04e6db489c282f05a7009a2cae
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 15:55:47 +0200

    Completed NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 	int number = Integer.parseInt(args[0]);
-
+	// extracting and printing out the hundres, tens and ones.
 	System.out.println(number / 100 + " hundreds, " + (number % 100)/10 +
 						" tens, " + number % 10 + " ones."); 
 	}
