60a0416e0c7ed9549bb2f89ed3fd1cf0642738af
shachaf-steinberg <Shachaf.steinberg@post.runi.ac.il>
2024-11-06 16:05:37 +0200

    Completed TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,7 +3,7 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-
+		// Deduct 12 if hours is bigger then 13 to get 12-hour AM/PM format.
 		if (hours >= 13) {
 			hours -= 12;
 		} 
